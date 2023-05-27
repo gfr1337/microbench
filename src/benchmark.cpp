09c9f2e5b2a2ecb431ec4a3fc66fe62274345ef5
George Rogers <george.f.rogers@gmail.com>
2023-05-27 04:25:47 -0400

    update

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -1,5 +1,7 @@
 #include <chrono>
-#include <fmt/core.h>
+#include <iostream>
+#include <fmt/chrono.h>
+#include <fmt/ostream.h>
 #include <picalc.h>
 #include <benchmark.h>
 
@@ -7,10 +9,10 @@ namespace picalc {
     void benchmark(int64_t n, int64_t e) {
         auto a = std::chrono::high_resolution_clock::now();
         for(int64_t i = 0; i < n; i++) {
-            fmt::print("{:16.9f}\n", compute_pi(e));
+            fmt::print(std::cout, "{:16.9f}\n", compute_pi(e));
         }
         auto b = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double, std::ratio<1, 1>> c = b-a;
-        fmt::print("{} seconds\n", c.count());
+        fmt::print(std::cerr, "{}\n", c);
     }
 }
