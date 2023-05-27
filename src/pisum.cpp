8c4868319a9a1103e1cf2e2c68fe39afc7318149
George Rogers <george.f.rogers@gmail.com>
2023-05-27 10:48:10 -0400

    update

--- src/pisum.cpp
+++ src/pisum.cpp
@@ -4,18 +4,9 @@
 #include <vector>
 
 int main(int argc, char **argv) {
-    std::vector<std::string> args;
-    for(int i = 1; i < argc; i++) {
-        args.push_back(argv[i]);
-    }
-    if(args.size() < 2) {
-        std::cerr << "less than 2 arguments given\n";
-        exit(1);
-    }
-    int64_t n = stoll(args.at(0));
-    int64_t e = stoll(args.at(1));
-    for(int i = 0; i < n; i++) {
-        picalc::benchmark(100, e);
+    std::cout << "C++\n";
+    for(int i = 0; i < 10; i++) {
+        picalc::benchmark(100, 30000000);
     }
     return 0;
 }
